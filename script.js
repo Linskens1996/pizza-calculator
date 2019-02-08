2eb736daab16b04e2d44517171e0e926e5b94a58
Dennis Linskens <99017722@mydavinci.nl>
2019-02-08 17:22:39 +0100

    .

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Dennis Linskens - Pizza calculator
+
 var pizzas = [
   {
     name: "Pizza BBQ Mixed Grill",
@@ -21,35 +23,70 @@ var pizzas = [
   }
 ];
 
+// -----------------------------------------------------------------------------------------------------------------------
+// Show the content of the pizzas-array on the homepage and in the modal-boxes -------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
 for (let i = 0; i < pizzas.length; i++) {
   const pizzasarray = pizzas[i];
   var idNumber = i + 1;
+
   var headerText = "document.getElementById('pizzaheader" + idNumber + "').innerHTML = " + "\'" + pizzasarray.name + "\'";
+  var pizzaPrice = "document.getElementById('pizzaprice" + idNumber + "').innerHTML = " + "\'" + "€ " + pizzasarray.price + "\'";
+  var imageSRC = "document.getElementById('pizzaimage" + idNumber + "').src = " + "\'" + pizzasarray.image + "\'";
+  var headerTextModal = "document.getElementById('pizzaheader" + idNumber + "modal').innerHTML = " + "\'" + pizzasarray.name + "\'";
+
   eval(headerText);
+  eval(pizzaPrice);
+  eval(headerTextModal);
+  eval(imageSRC);
 }
 
-function imageFunction() {
-  var image1 = document.getElementById("pizzaimage1"),
-      image2 = document.getElementById("pizzaimage2"),
-      image3 = document.getElementById("pizzaimage3"),
-      image4 = document.getElementById("pizzaimage4");
+var toppings = [
+  {
+    name: "Tonijn",
+    price: 1.50
+  },
+  {
+    name: "Pulled beef",
+    price: 1.50
+  },
+  {
+    name: "Knoflooksaus swirl",
+    price: 1.00
+  },
+  {
+    name: "Spaanse pepers",
+    price: 1.00
+  },
+  {
+    name: "Lente ui",
+    price: 0.50
+  },
+  {
+    name: "Paprikamix",
+    price: 0.50
+  }
+];
 
-  image1.src = pizzas[0].image;
-  image2.src = pizzas[1].image;
-  image3.src = pizzas[2].image;
-  image4.src = pizzas[3].image;
-};
-imageFunction();
+// -----------------------------------------------------------------------------------------------------------------------
+// Show toppings in the modal boxes --------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+for (let i = 0; i < toppings.length; i++) {
+  const toppingsarray = toppings[i];
 
-function priceFunction() {
-  var price1 = document.getElementById("pizzaprice1"),
-      price2 = document.getElementById("pizzaprice2"),
-      price3 = document.getElementById("pizzaprice3"),
-      price4 = document.getElementById("pizzaprice4");
+  var checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = "id";
+  checkbox.className = "w3-check";
 
-  price1.innerHTML = "€ " + pizzas[0].price;
-  price2.innerHTML = "€ " + pizzas[1].price;
-  price3.innerHTML = "€ " + pizzas[2].price;
-  price4.innerHTML = "€ " + pizzas[3].price;
-};
-priceFunction();
+  var tn = " " + toppingsarray.name + " + € " + toppingsarray.price;
+  var label = document.createElement("label");
+  label.htmlFor = "id";
+  label.appendChild(document.createTextNode(tn));
+
+  var br = document.createElement("br");
+  label.appendChild(br);
+
+  document.getElementById("modal-toppings1").appendChild(checkbox);
+  document.getElementById("modal-toppings1").appendChild(label);
+}
